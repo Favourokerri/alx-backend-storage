7c402dbb2d2ed66fdf64edaa70f778a4c76d7134
Favourokerri <favourokerri767@gmail.com>
2023-12-20 13:16:21 +0000

    exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Writing strings to Redis"""
+import redis
+import uuid
+
+
+class Cache:
+    """ cache class """
+    def __init__(self):
+        """ instance of redis """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str) -> str:
+        """
+            store method with agument data
+            sets the value in our redis and
+            return the key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
